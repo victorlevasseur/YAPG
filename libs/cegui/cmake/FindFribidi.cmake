ce4281948d966655ef7bb09826b8587a88229997
Victor Levasseur <victorlevasseur52@gmail.com>
2016-02-19 16:09:16 +0100

    Remove CEGUI files

--- libs/cegui/cmake/FindFribidi.cmake
+++
@@ -1,7 +0,0 @@
-################################################################################
-# Custom cmake module for CEGUI to find the fribidi library & header
-################################################################################
-include(CEGUIFindHelpers)
-
-cegui_find_package(FRIBIDI fribidi fribidi.h fribidi fribidi) 
-
