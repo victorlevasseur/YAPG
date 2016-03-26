0132642de295f24e8fc319093ec24f3ac811ccb4
Victor Levasseur <victorlevasseur52@gmail.com>
2016-03-26 12:51:43 +0100

    Fix player still moving after it finished the level

--- game/src/Systems/PlayerSystem.cpp
+++ game/src/Systems/PlayerSystem.cpp
@@ -26,9 +26,12 @@ void PlayerSystem::update(entityx::EntityManager &es, entityx::EventManager &eve
         const settings::KeySettings::PlayerKeys& playerKeys =
             m_settingsManager.getKeySettings().getPlayerKeys(player.playerNumber);
 
-        platformer.wantsToGoLeft = sf::Keyboard::isKeyPressed(playerKeys.leftKey);
-        platformer.wantsToGoRight = sf::Keyboard::isKeyPressed(playerKeys.rightKey);
-        platformer.wantsToJump = sf::Keyboard::isKeyPressed(playerKeys.jumpKey);
+        if(!player.finishedLevel)
+        {
+            platformer.wantsToGoLeft = sf::Keyboard::isKeyPressed(playerKeys.leftKey);
+            platformer.wantsToGoRight = sf::Keyboard::isKeyPressed(playerKeys.rightKey);
+            platformer.wantsToJump = sf::Keyboard::isKeyPressed(playerKeys.jumpKey);
+        }
     });
 }
 
