314c97ccf7f48bf30b5b91de187197f96d87e346
Victor Levasseur <victorlevasseur52@gmail.com>
2016-04-08 15:41:37 +0200

    Add another player template

---
+++ game/resources/templates/kenney/player2.lua
@@ -0,0 +1,43 @@
+entity_template = {
+    name = "kenney_player2",
+    friendlyname = "[Kenney] Player 2",
+    inherits = "kenney_player1",
+
+    components = {
+        ["Render"] = {
+            animations = {
+                ["idle"] = {
+                    total_duration = 1,
+                    frames = {
+                        {
+                            rect = { left = 650, top = 516, width = 128, height = 256},
+                            relative_duration = 1,
+                        },
+                    },
+                },
+                ["walking"] = {
+                    total_duration = 0.2,
+                    frames = {
+                        {
+                            rect = { left = 520, top = 1548, width = 128, height = 256},
+                            relative_duration = 1,
+                        },
+                        {
+                            rect = { left = 520, top = 1290, width = 128, height = 256},
+                            relative_duration = 1,
+                        },
+                    },
+                },
+                ["jump"] = {
+                    total_duration = 1,
+                    frames = {
+                        {
+                            rect = { left = 650, top = 774, width = 128, height = 256},
+                            relative_duration = 1,
+                        },
+                    },
+                },
+            },
+        }
+    }
+}
